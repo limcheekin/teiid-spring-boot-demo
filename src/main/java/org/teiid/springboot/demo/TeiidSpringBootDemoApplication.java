ebbbb4fc818505a35c994de0f545b9d301d9f4ed
Lim, Chee Kin <chee-kin.lim@dxc.com>
2019-11-15 11:38:59 +0800

    first import

---
+++ src/main/java/org/teiid/springboot/demo/TeiidSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package org.teiid.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeiidSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeiidSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/teiid/springboot/demo/TeiidSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teiid.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeiidSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
